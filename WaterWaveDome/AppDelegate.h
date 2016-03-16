6b04854e638e8064d1d90236f212369fd77ba951
tiaotiaoxin <891783557@qq.com>
2016-03-16 11:39:16 +0800

    Initial Commit

---
+++ WaterWaveDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaterWaveDome
+//
+//  Created by 123 on 16/3/16.
+//  Copyright © 2016年 YM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaterWaveDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaterWaveDome
+//
+//  Created by 123 on 16/3/16.
+//  Copyright © 2016年 YM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
